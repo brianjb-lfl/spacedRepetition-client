a108ecd118271a7d47e494afe4bb03a670967d87
Brian Besaw <bjbesaw@outlook.com>
2018-01-11 20:21:23 -0500

    Corrected base api url.

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = 'https://p-code-server.herokuapp.com/';
+export const API_BASE_URL = 'https://p-code-server.herokuapp.com/api';
 
 //export const API_BASE_URL = 'http://localhost:8080/api';
 
